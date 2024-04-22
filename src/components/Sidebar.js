9d9402d1cb725c1440802496aa243965e3bbe2e2
Himanshu Sharma <55379950+hsharma123@users.noreply.github.com>
2024-04-22 10:49:29 +0530

    overbilling page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,210 +1,61 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import logoOpen from '../assets/images/logo.png';
+// Sidebar.js
+import React from 'react';
+import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { Link } from 'react-router-dom';
-import logoClosed from '../assets/images/logo.png';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import SsidChartIcon from '@mui/icons-material/SsidChart';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import SsidChartIcon from '@mui/icons-material/SsidChart';
 import PieChartOutlineIcon from '@mui/icons-material/PieChartOutline';
 import ScoreOutlinedIcon from '@mui/icons-material/ScoreOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
-import { Grid, FormControl, Select, MenuItem, Box, Stack } from '@mui/material';
+import logo from "../assets/images/logo.png";
 
 const drawerWidth = 240;
+const closedDrawerWidth = 65;
 
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: 65,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme, open }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-
-  transition: 'width 225ms cubic-bezier(0, 0, 0.2, 1) 0ms',
-}));
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  backgroundColor: '#fff',
-  boxShadow: 'none',
-  borderBottom: '1px solid #ccc',
-  width: 'calc(100% - 65px)', 
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
-  ...(open && {
-    ...openedMixin(theme),
-    '& .MuiDrawer-paper': openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    '& .MuiDrawer-paper': closedMixin(theme),
-  }),
-}));
-
-const LogoClosed = styled('img')(({ open }) => ({
-  width: open ? '70px' : '36px', 
-}));
-const SelectClosed = styled(Select)(({ open }) => ({
-  width: open ? '250px' : '300px',
-}));
-export default function MiniDrawer() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setOpen(!open);
-  };
-
-  const iconArray = [<DashboardOutlinedIcon />, <SsidChartIcon />, <PieChartOutlineIcon />, <ScoreOutlinedIcon />];
+const Sidebar = ({ isOpen }) => {
+  const sidebarItems = [
+    { text: 'Dashboard', icon: <DashboardOutlinedIcon />, link: '/dashboard' },
+    { text: 'OverBilling', icon: <SsidChartIcon />, link: '/overbilling' },
+    { text: 'Shortage Claim', icon: <PieChartOutlineIcon />, link: '/shortage-claim' },
+    { text: 'Financial Scorecard', icon: <ScoreOutlinedIcon />, link: '/financial-scorecard' },
+  ];
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label={open ? "close drawer" : "open drawer"}
-            onClick={handleDrawerToggle}
-            edge="start" className='nav-header-icon'
-          >
-            <MenuIcon />
-          </IconButton>
-          <Grid container rowSpacing={2}>
-            <Grid item xs={4} className="ps-5">
-              <FormControl>
-              <SelectClosed  value={10} displayEmpty className='form-control'>
-
-                  <MenuItem value={10}><PersonOutlineOutlinedIcon className='icons' />  US Demo Account</MenuItem>
-                  <MenuItem value={20}><PersonOutlineOutlinedIcon className='icons' />  US Demo Account</MenuItem>
-                  <MenuItem value={30}><PersonOutlineOutlinedIcon className='icons' />  US Demo Account</MenuItem>
-                </SelectClosed>
-              </FormControl>
-            </Grid>
-            <Grid item xs={2}>
-              <FormControl>
-                <Select value={10} displayEmpty className='form-control form-select'>
-                  <MenuItem value={10} >USD</MenuItem>
-                  <MenuItem value={20} >USD</MenuItem>
-                  <MenuItem value={30} >USD</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={6}>
-              <Box className="nav-header-right">
-                <Stack direction="row" spacing={2}>
-                  <MenuItem><EmailOutlinedIcon /></MenuItem>
-                  <MenuItem><NotificationsNoneOutlinedIcon /></MenuItem>
-                  <MenuItem><Person2OutlinedIcon /></MenuItem>
-                </Stack>
-              </Box>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader
-          className='nav-header-left' >
-          <LogoClosed src={open ? logoOpen : logoClosed} alt='Logo' className='logo' open={open} />
-        </DrawerHeader>
-        <Divider />
-        <List className='sidebar-list'>
-          {[
-            { text: 'Dashboard', icon: <DashboardOutlinedIcon /> },
-            { text: 'OverBilling', icon: <SsidChartIcon /> },
-            { text: 'Shortage Claim', icon: <PieChartOutlineIcon /> },
-            { text: 'Financial Scorecard', icon: <ScoreOutlinedIcon /> },
-          ].map((item, index) => (
-            <ListItem key={item.text} disablePadding sx={{ display: 'block' }} className='sidebar-list-item'>
-            <Link
-  to={`/${item.text.toLowerCase().replace(' ', '-')}`}
-  style={{ textDecoration: 'none', color: 'inherit' }}
->
-  <ListItemButton
-    sx={{
-      minHeight: 48,
-      justifyContent: open ? 'initial' : 'center',
-      px: 2.5,
-    }}
-  >
-    <ListItemIcon
-      className='sidebar-list-icon'
+    <Drawer
+      variant="permanent"
       sx={{
-        minWidth: 0,
-        mr: open ? 3 : 'auto',
-        justifyContent: 'center',
+        width: isOpen ? drawerWidth : closedDrawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: isOpen ? drawerWidth : closedDrawerWidth,
+          transition: 'width 0.3s ease',
+          overflowX: 'hidden',
+        },
       }}
+      open={isOpen}
     >
-      {item.icon}
-    </ListItemIcon>
-    <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
-  </ListItemButton>
-</Link>
-
-            </ListItem>
-          ))}
-        </List>
-
-
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader open={open} />
-      </Box>
-    </Box>
-
+      <List className="sidebar-list">
+        <div style={{ textAlign: 'center', padding: '20px 0' }}>
+          <Link to="/">
+            <img src={logo} alt="Logo" style={{ width: isOpen ? '70px' : '40px', transition: 'width 0.3s ease' }} />
+          </Link>
+        </div>
+        {sidebarItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: 'block' }} className="sidebar-list-item">
+            <ListItemButton sx={{ minHeight: 48 }} component={Link} to={item.link}>
+              <ListItemIcon className='sidebar-list-item-icon'>{item.icon}</ListItemIcon>
+              <ListItemText
+                primary={item.text}
+                sx={{
+                  opacity: isOpen ? 1 : 0,
+                  transition: 'opacity 0.3s ease',
+                }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
-}
+};
+
+export default Sidebar;
--- src/pages/overbilling/OverBilling.js
+++ src/pages/overbilling/OverBilling.js
@@ -1,57 +1,284 @@
-import React from 'react'
-import { Grid, Box, Container, Breadcrumbs,Typography} from '@mui/material';
+import React from 'react';
+import { Grid, Box, Container, Breadcrumbs, Typography, Card, CardContent, Stack,Button,Avatar, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
 import man from "../../assets/images/overbilling.png";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
 import { Link } from 'react-router-dom';
-
+import piggy from '../../assets/images/1.gif';
+import AreaGraph from '../../components/Graph/Area';
 
 const OverBilling = () => {
   return (
-   <Box className="overbilling">
-      <Container maxWidth="xxl">
-        <Grid container spacing={2}>
+    <Box className="overbilling">
+      <Container maxWidth="xl">
+        <Grid container spacing={3}>
           <Grid item xs={12}>
-          <Breadcrumbs aria-label="breadcrumb">
-          <Link
-            underline="hover"
-            sx={{ display: 'flex', alignItems: 'center' }}
-            color="inherit"
-            to="/"
-          >
-            <DashboardOutlinedIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-          Dashboard
-          </Link>
-          <Link
-            underline="hover"
-            sx={{ display: 'flex', alignItems: 'center' }}
-            color="inherit"
-            href="/overbilling"
-          >
-            OverBilling
-          </Link>
-        </Breadcrumbs>
+            <Breadcrumbs aria-label="breadcrumb" className='breadcrumb'>
+              <Link
+                underline="hover"
+                sx={{ display: 'flex', alignItems: 'center' }}
+                color="inherit"
+                to="/"
+              >
+                <DashboardOutlinedIcon sx={{ mr: 0.5 }} />
+                Dashboard
+              </Link>
+              <Link
+                underline="hover"
+                sx={{ display: 'flex', alignItems: 'center' }}
+                className="active"
+                href="/overbilling"
+              >
+                OverBilling
+              </Link>
+            </Breadcrumbs>
           </Grid>
-        
         </Grid>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12}>
-            <Box className="overbilling-header">
-              <Grid  container spacing={2}>
-                <Grid item xs={6}>
-                  <Typography variant="h4">Excess Coop Billing</Typography>
-                </Grid>
-                <Grid item xs={6} className="text-end">
-                  <img src={man} alt="man-laptop" className='overbilling-header-image' />
-                </Grid>
-              </Grid>
-            </Box>
+        <Box className="overbilling-header">
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="h5">Excess Coop Billing</Typography>
+            </Grid>
+            <Grid item xs={6} className="text-end">
+              <img src={man} alt="man-laptop" className='overbilling-header-image' />
+            </Grid>
           </Grid>
-        
-        </Grid>
-        
+        </Box>
+        <Box marginTop={3}>
+          <Grid container spacing={2} >
+            <Grid item xs={8}>
+              <Stack   direction={{ xs: 'column', sm: 'row' }} spacing={2} className="mb-3">
+                <Card className='card'>
+                  <CardContent className="pb-0 card-content">
+                    <Stack direction='row' spacing={2} useFlexGap justifyContent="space-between">
+                      <Box>
+                        <Typography variant='h5' fontSize="16px" className='text-accent' marginBottom={1}>Finding Amount</Typography>
+                        <Typography variant='body2' className='text-accent'><b>$ 98442</b></Typography>
+                      </Box>
+                    </Stack>
+                    <Stack direction='row' spacing={4} useFlexGap  justifyContent="flex-end" alignItems="flex-end">
+                      <Box>
+                         <img src={piggy} alt='piggy' className='card-img'/>
+                      </Box>
+                    </Stack>
+                  </CardContent>
+                </Card>
+                <Card className='card'>
+                  <CardContent className="pb-0 card-content">
+                    <Stack direction='row' spacing={3} useFlexGap justifyContent="space-between">
+                      <Box>
+                        <Typography variant='h5' fontSize="16px" className='text-primary' marginBottom={1}>Yearly Trending</Typography>
+                        <Typography variant='h6' className='text-primary'><b>2024</b></Typography>
+                        <Typography variant='body2' className='text-dark' marginBottom={1}>$ 280,966</Typography>
+                        
+                      </Box>
+                      <Box>
+                        <Button size="small" className='btn-sm' fullWidth>View Trends <ChevronRightIcon /></Button>
+                      </Box>
+                    </Stack>
+                    <Stack direction='row' spacing={4} useFlexGap marginTop={1}>
+                          <Box>
+                            <Typography variant='body1' className='text-accent'><b>2022</b></Typography>
+                            <Typography variant='body2' className='text-dark' >$ 1,280,966.35</Typography>
+                          </Box>
+                          <Box>
+                            <Typography variant='body1' className='text-accent'><b>2023</b></Typography>
+                            <Typography variant='body2' className='text-success'>$ 1,680,966.35</Typography>
+                          </Box>
+                      </Stack>
+                  </CardContent>
+                </Card>
+              </Stack>
+              <Stack direction={{ xs: 'column', sm: 'row' }}spacing={2}>
+                <Card className='card'>
+                  <CardContent className="pb-0 card-content">
+                    <Stack direction='row' spacing={2} useFlexGap justifyContent="space-between">
+                      <Box>
+                        <Typography variant='h5' fontSize="16px" className='text-primary' marginBottom={1}>Disputed Amount</Typography>
+                        <Typography variant='body2' className='text-primary' marginBottom={1}><b>$ 4,942,234.43</b></Typography>
+                        <Typography variant='body2' className='text-dark'>View all raised disputes till date</Typography>
+                      </Box>
+                      <Box>
+                        <Button size="small" className='btn-sm' fullWidth>View Details <ChevronRightIcon /></Button>
+                      </Box>
+                    </Stack>
+                  </CardContent>
+                </Card>
+                <Card className='card'>
+                  <CardContent className="pb-0 card-content">
+                    <Stack direction='row' spacing={3} useFlexGap justifyContent="space-between">
+                      <Box>
+                        <Typography variant='h5' fontSize="16px" className='text-success' marginBottom={1}>Disputed Amount</Typography>
+                        <Typography variant='body2' className='text-success' marginBottom={1}><b>$ 4,942,234.43</b></Typography>
+                      </Box>
+                    </Stack>
+                    <Stack direction='row' spacing={4} useFlexGap  justifyContent="flex-end" alignItems="flex-end">
+                      < Box>
+                      <AreaGraph />
+                      </Box>
+                    </Stack>
+                    
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+         
+            <Grid item xs={4}>
+              <Card className='card'>
+                  <CardContent className="pb-0 card-content">
+                    <Stack direction='row' spacing={3} useFlexGap justifyContent="space-between">
+                      <Box>
+                        <Typography variant='h6' className='text-secondary' marginBottom={1}>Impact Overview</Typography>
+                        <Typography variant='body2' className='text-accent' marginBottom={1}>Recouped Amount</Typography>
+                        <Typography variant='body2' className='text-accent' marginBottom={1}><b>$ 89548</b></Typography>
+                      </Box>
+                      <Box>
+                      <div style={{ width: '70px', height: '70px' }}>
+                        <svg className="CircularProgressbar" viewBox="0 0 100 100" data-test-id="CircularProgressbar">
+                         <path className="CircularProgressbar-trail" d="M 50,50 m 0,-46 a 46,46 0 1 1 0,92 a 46,46 0 1 1 0,-92" strokeWidth="8" fillOpacity="0" style={{ stroke: 'rgba(255, 135, 31,0.1)', strokeLinecap: 'round', transform: 'rotate(0.7turn)', transformOrigin: 'center center', strokeDasharray: '289.027px, 289.027px', strokeDashoffset: '0px' }}></path>
+                         <path className="CircularProgressbar-path" d="M 50,50 m 0,-46 a 46,46 0 1 1 0,92 a 46,46 0 1 1 0,-92" strokeWidth="8" fillOpacity="0" style={{ stroke: 'rgb(255, 135, 31)', strokeLinecap: 'round', transform: 'rotate(0.7turn)', transformOrigin: 'center center', transitionDuration: '0.5s', strokeDasharray: '289.027px, 289.027px', strokeDashoffset: '65.031px' }}></path>
+                         <text className="CircularProgressbar-text" x="50" y="53" textAnchor="middle" style={{ fill: 'rgb(255, 135, 31)', fontSize: '18px' }}>77.5</text>
+                       </svg>
+                      </div>
+                      </Box>
+                    </Stack>
+                    <Stack direction='column'marginTop={3}>
+                      <Stack direction='row'spacing={4} alignItems="center" justifyContent="space-between" className='mb-3'>
+                        <Stack direction='row' spacing={1} alignItems="center">
+                          <Avatar className='bg-success' sx={{ width: 30, height: 30 }}>
+                            <AccessTimeOutlinedIcon />
+                          </Avatar>
+                          <Box>
+                            <Typography variant='h6' className='card-text text-dark'>Hour Saved</Typography>
+                          </Box>
+                        </Stack>
+                        <Typography  variant='h6' className='card-text text-dark'>18</Typography>
+                      </Stack>
+                      <Stack direction='row'spacing={4} alignItems="center" justifyContent="space-between" className='mb-3'>
+                        <Stack direction='row'spacing={1} alignItems="center">
+                          <Avatar className='bg-accent' sx={{ width: 30, height: 30 }}>
+                            <BusinessCenterOutlinedIcon />
+                          </Avatar>
+                          <Box>
+                            <Typography variant='h6' className='card-text text-dark'>Hour Saved</Typography>
+                          </Box>
+                        </Stack>
+                        <Typography  variant='h6' className='card-text text-dark'>18</Typography>
+                      </Stack>
+                      <Stack direction='row'spacing={4} alignItems="center" justifyContent="space-between" className='mb-3'>
+                        <Stack direction='row'spacing={1} alignItems="center">
+                          <Avatar className='bg-primary' sx={{ width: 30, height: 30 }}>
+                            <AttachMoneyOutlinedIcon />
+                          </Avatar>
+                          <Box>
+                            <Typography variant='h6' className='card-text text-dark'>Money Saved</Typography>
+                          </Box>
+                        </Stack>
+                        <Typography  variant='h6' className='card-text text-dark'>18</Typography>
+                      </Stack>       
+                    </Stack>    
+                            
+                  </CardContent>
+                </Card>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box marginTop={2}>
+          <Grid container spacing={2} >
+            <Grid item xs={6}>
+                <Card className='card'>
+                  <CardContent className="pb-0 card-content">
+                     <Typography variant='h6' className='card-header'>Granular Analysis</Typography>
+                           <Table className='table'>
+                              <TableHead className='bg-primary text-white'>
+                                <TableRow>
+                                 <TableCell className='text-white'>Sub-type</TableCell>
+                                 <TableCell className='text-white'>Findings $</TableCell>
+                                 <TableCell className='text-white'>Prior Adjustments $</TableCell>
+                                 <TableCell className='text-white'>Net Off</TableCell>
+                                </TableRow>
+                              </TableHead>
+                               <TableBody>
+                                  <TableRow>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                  <TableRow className='bg-info'>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                  <TableRow>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                  <TableRow className='bg-info'>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                </TableBody>
+                            </Table>
+                  </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={6}>
+            <Card className='card'>
+                  <CardContent className="pb-0 card-content">
+                     <Typography variant='h6' className='card-header'>Win Rate</Typography>
+                           <Table className='table'>
+                              <TableHead className='bg-primary text-white'>
+                                <TableRow>
+                                 <TableCell className='text-white'>Sub-type</TableCell>
+                                 <TableCell className='text-white'>Findings $</TableCell>
+                                 <TableCell className='text-white'>Prior Adjustments $</TableCell>
+                                 <TableCell className='text-white'>Net Off</TableCell>
+                                </TableRow>
+                              </TableHead>
+                               <TableBody>
+                                  <TableRow>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                  <TableRow className='bg-info'>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                  <TableRow>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                  <TableRow className='bg-info'>
+                                   <TableCell>Duplicate Freight</TableCell>
+                                   <TableCell>362,356.00</TableCell>
+                                   <TableCell>65,231.00</TableCell>
+                                   <TableCell>6547.25</TableCell>
+                                  </TableRow>
+                                </TableBody>
+                            </Table>
+                  </CardContent>
+                </Card>
+            </Grid>
+          </Grid>
+        </Box>
       </Container>
-   </Box>
-  )
+    </Box>
+  ); 
 }
 
 export default OverBilling;
